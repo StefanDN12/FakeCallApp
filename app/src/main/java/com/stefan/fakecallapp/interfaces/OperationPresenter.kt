b327ced5e638722c114cf1d37cfb686ffb794a35
Stefan <estefan_daniel100@hotmail.com>
2023-02-11 17:26:43 +0100

    Presenter implementation

--- app/src/main/java/com/stefan/fakecallapp/interfaces/OperationPresenter.kt
+++ app/src/main/java/com/stefan/fakecallapp/interfaces/OperationPresenter.kt
@@ -2,4 +2,5 @@ package com.stefan.fakecallapp.interfaces
 
 interface OperationPresenter {
     fun goCall(number: String)
+    fun showNumbre()
 }
--- app/src/main/java/com/stefan/fakecallapp/presenter/FakeAppPresenter.kt
+++ app/src/main/java/com/stefan/fakecallapp/presenter/FakeAppPresenter.kt
@@ -2,9 +2,18 @@ package com.stefan.fakecallapp.presenter
 
 import com.stefan.fakecallapp.interfaces.OperationPresenter
 import com.stefan.fakecallapp.interfaces.OperationView
+import com.stefan.fakecallapp.model.FakeAppModel
 
 class FakeAppPresenter(view: OperationView):OperationPresenter {
+
+    var view = view
+    var model = FakeAppModel()
+
     override fun goCall(number: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showNumbre() {
         TODO("Not yet implemented")
     }
 
