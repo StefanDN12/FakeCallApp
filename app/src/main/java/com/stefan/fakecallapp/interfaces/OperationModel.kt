c809463bf220449a7e27ff3aa2b158b6334c14ee
Stefan <estefan_daniel100@hotmail.com>
2023-02-18 12:22:37 +0100

    Adds permission for call in model

--- app/src/main/java/com/stefan/fakecallapp/interfaces/OperationModel.kt
+++ app/src/main/java/com/stefan/fakecallapp/interfaces/OperationModel.kt
@@ -1,7 +1,8 @@
 package com.stefan.fakecallapp.interfaces
 
+import android.app.Activity
 import android.content.Context
 
 interface OperationModel {
-    fun makeCall(number:String, context: Context)
+    fun makeCall(number:String, context: Activity)
 }
--- app/src/main/java/com/stefan/fakecallapp/model/FakeAppModel.kt
+++ app/src/main/java/com/stefan/fakecallapp/model/FakeAppModel.kt
@@ -1,8 +1,14 @@
 package com.stefan.fakecallapp.model
 
+import android.Manifest
+import android.Manifest.permission.CALL_PHONE
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.stefan.fakecallapp.interfaces.OperationModel
 import com.stefan.fakecallapp.interfaces.OperationPresenter
 import com.stefan.fakecallapp.presenter.FakeAppPresenter
@@ -13,9 +19,15 @@ class FakeAppModel(presenter: FakeAppPresenter):OperationModel {
     private var  intent: Intent = Intent(Intent.ACTION_CALL)
 
 
-    override fun makeCall(number: String, context: Context) {
+    override fun makeCall(number: String, context: Activity) {
         intent.setData(Uri.parse("tel:"+ number.toInt()))
-        context.startActivity(intent)
+
+        if(ContextCompat.checkSelfPermission(context,CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+            context.startActivity(intent)
+        }else{
+            context.requestPermissions(arrayOf(CALL_PHONE),1)
+        }
+
         presenter.showNumbre(number)
     }
 }
